49a7607c9c7f50372e0667d1465e2aad876ab5ec
brotifypacha <brotifypacha@gmail.com>
2025-12-27 21:23:56 +0300

    feat(test-config): add helper mappings

--- tests/test-config.lua
+++ tests/test-config.lua
@@ -5,13 +5,20 @@ vim.cmd [[
   set runtimepath+=~/.local/share/nvim/site/pack/packer/start/telescope.nvim
 ]]
 
-vim.keymap.set("n", "<F1><F1>", function() require("tabs-as-projects").pick_project({
+local tabs_as_projects = require("tabs-as-projects")
+
+vim.keymap.set("n", "<F1><F1>", tabs_as_projects.pick_project({
   search_dirs = {
     { category = "home", path = "~" },
   },
   pick_cmd = "tcd",
-}) end)
+}))
 
 -- always show tabline
 vim.o.showtabline = 2
 vim.o.tabline = "%{%v:lua.require('tabs-as-projects.ui').tabline()%}"
+
+-- herlper mappings
+vim.keymap.set("n", "<space>tt", "<cmd>tabnew<cr>")
+vim.keymap.set("n", "[[", "<cmd>tabprev<cr>")
+vim.keymap.set("n", "]]", "<cmd>tabnext<cr>")
