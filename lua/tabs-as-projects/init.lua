436a2515ec710ae9c09fce9e6b5051c0469549ab
brotifypacha <brotifypacha@gmail.com>
2026-01-02 15:29:01 +0300

    feat(ui): make close_icon customizable

--- lua/tabs-as-projects/init.lua
+++ lua/tabs-as-projects/init.lua
@@ -1,11 +1,27 @@
 require 'tabs-as-projects.health'
 
 local M = {
-  pick_project = function(opts)
-    return function()
-      require("tabs-as-projects.picker").pick_project(opts)
-    end
-  end
+  opts = {},
 }
+
+--- @class setup_options
+--- @field ui any
+
+--- @param opts setup_options
+function M.setup(opts)
+
+  M.opts = opts
+
+  if opts.ui ~= nil then
+    require("tabs-as-projects.ui").setup(opts.ui)
+  end
+
+end
+
+function M.pick_project(opts)
+  return function()
+    require("tabs-as-projects.picker").pick_project(opts)
+  end
+end
 
 return M
